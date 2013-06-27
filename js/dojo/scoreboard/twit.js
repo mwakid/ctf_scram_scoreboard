601773837fe0a766c714bf701386e41bffc406ce
Joe Pecsi <jpecsi@me.com>
2013-06-27 10:56:43 -0400

    Began editing twitter widget
    Created a page to test the twitter widget, started working on the
    ui.js, ui.html , twit.js, and twit.html files

--- js/dojo/scoreboard/twit.js
+++ js/dojo/scoreboard/twit.js
@@ -1,11 +1,10 @@
-define(["dojo/_base/lang", "dojo/_base/declare", "dijit/_WidgetBase", "dijit/_Container", "dijit/_Contained", "dijit/_TemplatedMixin", "scoreboard/twit", "dojo/text!scoreboard/templates/ui.html"], function(lang, Declare, _WidgetBase, _Container, _Contained, _TemplatedMixin, Twit, template) {
+define(["dojo/_base/lang", "dojo/_base/declare", "dijit/_WidgetBase", "dijit/_Container", "dijit/_Contained", "dijit/_TemplatedMixin", "dojo/text!scoreboard/templates/ui.html"], function(lang, Declare, _WidgetBase, _Container, _Contained, _TemplatedMixin, template) {
 	return Declare("scoreboard.ui", [_WidgetBase, _TemplatedMixin, _Contained, _Container], {
 		///
 		/// This is the class for the main UI
 		///
 		templateString : template,
 		args : null, //property ba
-		twit : null, 
 
 		constructor : function(args) {
 			this.args = args;
@@ -15,7 +14,7 @@ define(["dojo/_base/lang", "dojo/_base/declare", "dijit/_WidgetBase", "dijit/_Co
 			this.inherited(arguments);
 		},
 		postCreate : function() {
-			this.twit = new Twit({}, this.twitDAP);
+
 			this.inherited(arguments);
 		},
 		startup : function() {
