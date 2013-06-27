a64c84ecc31bd4d902c99e9e73e0213623d37290
j105rob <j105.rob@gmail.com>
2013-06-27 11:25:42 -0400

    Moved twitter js into the twit.js

--- js/dojo/scoreboard/twit.js
+++ js/dojo/scoreboard/twit.js
@@ -1,5 +1,5 @@
-define(["dojo/_base/lang", "dojo/_base/declare", "dijit/_WidgetBase", "dijit/_Container", "dijit/_Contained", "dijit/_TemplatedMixin", "dojo/text!scoreboard/templates/ui.html"], function(lang, Declare, _WidgetBase, _Container, _Contained, _TemplatedMixin, template) {
-	return Declare("scoreboard.ui", [_WidgetBase, _TemplatedMixin, _Contained, _Container], {
+define(["dojo/_base/lang", "dojo/_base/declare", "dijit/_WidgetBase", "dijit/_Container", "dijit/_Contained", "dijit/_TemplatedMixin", "dojo/text!scoreboard/templates/twit.html"], function(lang, Declare, _WidgetBase, _Container, _Contained, _TemplatedMixin, template) {
+	return Declare("scoreboard.twit", [_WidgetBase, _TemplatedMixin, _Contained, _Container], {
 		///
 		/// This is the class for the main UI
 		///
@@ -8,13 +8,22 @@ define(["dojo/_base/lang", "dojo/_base/declare", "dijit/_WidgetBase", "dijit/_Co
 
 		constructor : function(args) {
 			this.args = args;
-
+			
+		},
+		_setup : function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
+			if (!d.getElementById(id)) {
+				js = d.createElement(s);
+				js.id = id;
+				js.src = p + "://platform.twitter.com/widgets.js";
+				fjs.parentNode.insertBefore(js, fjs);
+			}
 		},
 		buildRendering : function() {
 			this.inherited(arguments);
 		},
 		postCreate : function() {
-
+			this._setup(document,"script","twitter-wjs");
 			this.inherited(arguments);
 		},
 		startup : function() {
